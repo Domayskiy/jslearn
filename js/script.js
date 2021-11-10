1035d8876324965832ba4da3bf44e485a205ef56
Domayskiy <a.domayskiy@gmail.com>
2021-11-10 21:49:34 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// alert('Hello');
+
+//const result = confirm("A you here&");
+//console.log(result);
+
+//const answer = prompt("Vam ect 18", "");
+//console.log(typeof(answer));
+
+const answers = [];
+answers[0] = prompt('kak vahe ima?', '');
+answers[1] = prompt('kak vahe famil?', '');
+answers[2] = prompt('kak vahe let?', '');
+
+document.write(answers);
+
+// const obj = {
+    //name: "John",
+    //age: 25,
+    //isMarried: false
+//};
+
+// console.log(obj.name);
+
+//let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+//console.log(arr[1]);
